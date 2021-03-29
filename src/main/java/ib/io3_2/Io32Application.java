f439e6323d07a40dd7e226cdb0bee1a02cf841ee
Grzegorz Stefański <stefanski.grzegorz6@gmail.com>
2021-03-29 10:24:28 +0200

    init

---
+++ src/main/java/ib/io3_2/Io32Application.java
@@ -0,0 +1,16 @@
+package ib.io3_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Io32Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Io32Application.class, args);
+
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/ib/io3_2/Io32ApplicationTests.java
@@ -0,0 +1,13 @@
+package ib.io3_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Io32ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
